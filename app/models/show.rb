cd011b5c005d07ec44bb22cbe9688dda4678c79a
Alex Wilson <fonzmp@gmail.com>
2018-06-05 19:10:35 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def Show::most_popular_show
-    show = Show.find_by(Show::highest_rating)
+    Show.find_by(Show::highest_rating)
   end
 
   def Show::lowest_rating
