23ca068be41255161020de27b18d0d8d08421952
Alex Wilson <fonzmp@gmail.com>
2018-06-05 18:45:30 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,6 @@
 class CreateShows < ActiveRecord::Migration
   def change
-    create_table :movies do |t|
+    create_table :shows do |t|
       t.string :title
       t.datetime :release_date
       t.string :director
